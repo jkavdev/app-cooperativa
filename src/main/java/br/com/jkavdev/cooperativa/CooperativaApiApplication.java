8a23c0e67e78e0599a038bf463b7dd7029dd0240
jkavdev <jhonatankolen@gmail.com>
2021-09-07 16:55:43 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/jkavdev/cooperativa/CooperativaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jkavdev.cooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jkavdev/cooperativa/CooperativaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jkavdev.cooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
