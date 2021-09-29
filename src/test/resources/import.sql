8fe8e6802d34ac9ce4f38d3e950949b5d9993bea
jkavdev <jhonatankolen@gmail.com>
2021-09-29 17:20:07 -0300

    - adicionando configuracao de contexto de test - adicionando alteracao no arquivos de requisicoes do postman

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO pauta(descricao) VALUES ('Spring Foda');
 
-INSERT INTO sessao(inicio, fim, pauta_id) VALUES (DATE_ADD(CURRENT_TIMESTAMP, interval 30 SECOND), DATE_ADD(CURRENT_TIMESTAMP, interval 60 SECOND), 1); 
+INSERT INTO sessao(inicio, fim, pauta_id) VALUES (DATEADD('SECOND',1, CURRENT_TIMESTAMP), DATEADD('SECOND',2, CURRENT_TIMESTAMP), 1); 
 
 INSERT INTO voto(cpf, sessao_id, voto) VALUES('50285724029', 1, false);
 INSERT INTO voto(cpf, sessao_id, voto) VALUES('54793936008', 1, false);
